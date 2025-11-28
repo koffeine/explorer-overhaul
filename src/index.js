54cf680ce55fee3fe1c90a320ea999ad59c8caf8
Kornél Horváth <kornel.horvath@me.com>
2025-11-28 18:04:27 +0100

    Convert to templates/vs-code

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/** @param {import('vscode').ExtensionContext} context */
+// eslint-disable-next-line no-empty-function, no-unused-vars
+export const activate = (context) => {
+};
+
+// eslint-disable-next-line no-empty-function
+export const deactivate = () => {};
